2887e8460e16a91a627469d014f75da8cd320d3b
Taufiq <taufiq9.2004@gmail.com>
2025-03-19 20:23:26 +0530

    templating setup

--- base/views.py
+++ base/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+rooms = [{'id':"1",'name':"lets explore !"},
+         {'id':"2",'name':"lets code !"},
+         {'id':"3",'name':"lets sing !"}]
+
 def home(request):
-    return HttpResponse("Hello Bhaiyo !")
+    return render(request, 'base/home.html')
 
 def room(request):
-    return render(request,'room.html')
+    return render(request,'base/room.html', {'rooms' : rooms})
 
 # Create your views here.
