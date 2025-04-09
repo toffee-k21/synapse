aebabec9de8ede095cbde8e063e2dc405a1457a8
Taufiq <taufiq9.2004@gmail.com>
2025-04-09 19:55:36 +0530

    update added

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room
+from .forms import RoomForm
 
 
 def home(request):
@@ -10,4 +11,25 @@ def home(request):
 def room(request,pk): 
     room = Room.objects.get(id=pk)
     return render(request,'base/room.html', {'room' : room})
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request, 'base/room-form.html', {'form': form})
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance = room)
+    
+    if request.method == 'POST':
+       form = RoomForm(request.POST, instance=room)
+       if form.is_valid():
+           form.save()
+           return redirect("home")
+       
+    return render(request, 'base/room-form.html', {'form': form})
 
