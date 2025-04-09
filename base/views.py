b667276bc165820a01778ea03b2859445d120e27
Taufiq <taufiq9.2004@gmail.com>
2025-04-09 23:03:20 +0530

    searching func

--- base/views.py
+++ base/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
-
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ""
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q))
     topics = Topic.objects.all()
-    return render(request, 'base/home.html', {'rooms' : rooms, 'topics': topics})
+    rooms_count = rooms.count()
+    return render(request, 'base/home.html', {'rooms' : rooms, 'topics': topics,'rooms_count':rooms_count})
 
 def room(request,pk): 
     room = Room.objects.get(id=pk)
