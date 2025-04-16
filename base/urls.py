a044cde8cd15ad3c29a6a90d9c2cd4d8d3c81d95
Taufiq <taufiq9.2004@gmail.com>
2025-04-16 19:15:18 +0530

    update profile feat

--- base/urls.py
+++ base/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('login/', views.loginPage , name = "login"),
     path('register/', views.registerUser , name = "register"),
     path('logout/', views.logoutUser , name = "logout"),
+    path('update-user/', views.updateUser , name = "update-user"),
 ]
--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db.models import Q
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
@@ -144,3 +144,14 @@ def deleteMessage(request, pk):
        
     return render(request, 'base/delete.html', {'obj': message})
 
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm( instance=user)
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    return render(request, 'base/update-user.html', {'form': form})
